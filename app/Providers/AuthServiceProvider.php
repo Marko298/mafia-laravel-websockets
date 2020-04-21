ca8b1fe6834d5b283abf48d39028046508882014
Mark Boychuk <boychuk.m.n@gmail.com>
2020-04-21 10:52:32 +0300

    updated auth for dashboard

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,6 +24,10 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        
+        Gate::define('viewWebSocketsDashboard', function ($user = null) {
+            return true;
+        });
 		
         //
     }
